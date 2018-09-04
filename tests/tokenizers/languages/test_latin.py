3282af863f3f3a7266f1149a7ec25a6372e5f50b
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-09-04 12:39:49 -0400

    add tests for computing frequencies

--- tests/tokenizers/languages/test_latin.py
+++ tests/tokenizers/languages/test_latin.py
@@ -15,7 +15,7 @@ from tesserae.tokenizers.languages import LatinTokenizer
 from tesserae.utils import TessFile
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='module')
 def latin_tokens(latin_files):
     tokens = []
     for fname in latin_files:
@@ -24,6 +24,21 @@ def latin_tokens(latin_files):
             ts = [t for t in json.load(f)]
             tokens.append(ts)
     return tokens
+
+
+@pytest.fixture(scope='module')
+def latin_word_frequencies(latin_files):
+    freqs = []
+    for fname in latin_files:
+        freq = {}
+        fname = os.path.splitext(fname)[0] + '.freq_score_word'
+        with open(fname, 'r') as f:
+            for line in f.readlines():
+                if '#' not in line:
+                    word, n = line.strip().split()
+                    freq[word] = int(n)
+        freqs.append(freq)
+    return freqs
 
 
 class TestLatinTokenizer(TestBaseTokenizer):
@@ -80,12 +95,13 @@ class TestLatinTokenizer(TestBaseTokenizer):
 
                 token_idx = offset
 
-    def test_tokenize(self, latin_files, latin_tokens):
+    def test_tokenize(self, latin_files, latin_tokens, latin_word_frequencies):
         la = self.__test_class__()
 
         for k in range(len(latin_files)):
             fname = latin_files[k]
             ref_tokens = [t for t in latin_tokens[k] if 'FORM' in t]
+            ref_freqs = latin_word_frequencies[k]
 
             t = TessFile(fname)
 
@@ -126,6 +142,18 @@ class TestLatinTokenizer(TestBaseTokenizer):
             la_tokens = [t for t in la.tokens
                       if re.search(r'^[a-zA-Z]+$', t.display, flags=re.UNICODE)]
 
+            correct = map(lambda x: x[0].display == x[1]['DISPLAY'],
+                          zip(la_tokens, ref_tokens))
+
+            print(len(la_tokens), len(ref_tokens))
+
+            if not all(correct):
+                for j in range(len(la_tokens)):
+                    if tokens[j].display != ref_tokens[j]['DISPLAY']:
+                        print('{}->{}'.format(la_tokens[j].display, ref_tokens[token_idx + j]['DISPLAY']))
+
+            assert all(correct)
+
             correct = map(lambda x: x[0].form == x[1]['FORM'],
                           zip(la_tokens, ref_tokens))
 
@@ -135,6 +163,27 @@ class TestLatinTokenizer(TestBaseTokenizer):
                 for j in range(len(la_tokens)):
                     if tokens[j].form != ref_tokens[j]['FORM']:
                         print('{}->{}'.format(la_tokens[j].form, ref_tokens[token_idx + j]['FORM']))
+
+            assert all(correct)
+
+            for key in ref_freqs:
+                assert key in la.frequencies
+                assert la.frequencies[key] == ref_freqs[key]
+
+            diff = []
+            for word in frequencies:
+                if word.form not in ref_freqs:
+                    diff.append(word.form)
+            print(diff)
+            assert len(diff) == 0
+
+            assert len(frequencies) == len(ref_freqs)
+            keys = sorted(list(ref_freqs.keys()))
+            frequencies.sort(key=lambda x: x.form)
+            correct = map(
+                lambda x: x[0].form == x[1] and
+                          x[0].frequency == ref_freqs[x[1]],
+                zip(frequencies, keys))
 
             assert all(correct)
 
