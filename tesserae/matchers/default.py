c8ff1b9a3e1222e2e89b5a7e1d6ffc0775f288c9
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-09-25 14:43:19 -0400

    base implementation of the v3 match enigne

--- tesserae/matchers/default.py
+++ tesserae/matchers/default.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from tesserae.db import get_connection
 
 # TODO: implement probabilistic stem matching once that's worked out
@@ -12,10 +14,26 @@ class DefaultMatcher(object):
         self.matches = []
 
     def frequency_dist(self, match_tokens):
-        pass
+        """Compute distance based on frequency.
 
-    def match(texts, stopwords=10, stopword_basis='corpus', score_basis='word',
-              frequency_basis='texts', max_distance=10,
+        The frequency distance computes the unmber of words separating the two
+        match words with the lowest frequency in the unit.
+
+        Parameters
+        ----------
+        match_tokens : list of (tesserae.db.Token, tesserae.db.Frequency)
+            The tokens to compute the distance.
+
+        Returns
+        -------
+        distance : float
+            The number of words separating the two lowest-frequency tokens.
+        """
+        match_tokens.sort(key: x[1].frequency)
+        return np.abs(match_tokens[0].index - match_tokens[1].index)
+
+    def match(texts, unit_type, stopwords=10, stopword_basis='corpus',
+              score_basis='word', frequency_basis='texts', max_distance=10,
               distance_metric='frequency'):
         """Find matches between one or more texts.
 
@@ -52,11 +70,51 @@ class DefaultMatcher(object):
         pass
 
     def retrieve_frequencies(self, texts, tokens, basis):
-        """
-        """
-        pass
+        """Get token frequencies for the tokens to be matched.
 
-    def retrieve_lines(self, text):
+        Parameters
+        ----------
+        texts : 'corpus' or list of tesserae.db.Text
+            The texts from which to compute the token frequencies. If 'corpus',
+            use token frequencies from the entire collection of texts in the
+            database. Otherwise, use frequencies within the specified texts.
+
+        tokens : list of tesserae.db.Token
+            The tokens that should be included in the match.
+        basis : {'word', 'stem'}
+
+
+        Returns
+        -------
+        frequencies : dict
+            Key/value pairs of frequencies indexed by text and token string:
+            {{
+                <text_id>: {{
+                    <token_string>: <frequency>
+                }}
+            }}
+        """
+        if basis == 'corpus':
+            frequencies = self.collection.find('frequencies',
+                                               form=[t.form for t in tokens])
+            formatted = {'corpus': {}}
+            for f in frequencies:
+                try:
+                    formatted['corpus'][f.form] += f.frequency
+                except KeyError:
+                    formatted['corpus'][f.form] = f.frequency
+        else:
+            frequencies = self.collection.find('frequencies',
+                                               text=[t.id for t in texts],
+                                               form=[t.form for t in tokens])
+
+            formatted = {t.cts_urn: {} for t in texts}
+            for f in frequencies:
+                formatted[f.text][f.form] = f.frequency
+
+        return formatted
+
+    def retrieve_lines(self, texts):
         """Get the lines associated with a text from the database.
 
         Parameters
@@ -69,9 +127,15 @@ class DefaultMatcher(object):
         lines : list of tesserae.db.Unit
             The line units in the order they appear in the original text.
         """
-        pass
+        lines = []
 
-    def retrieve_phrases(self, text):
+        for text in texts:
+            lines.append(connection.find('units',
+                         text=text.id, unit_type='line'))
+
+        return lines
+
+    def retrieve_phrases(self, texts):
         """Get the phrases associated with a text from the database.
 
         Parameters
@@ -84,9 +148,15 @@ class DefaultMatcher(object):
         phrases : list of tesserae.db.Unit
             The phrase units in the order they appear in the original text.
         """
-        pass
+        phrases = []
 
-    def retrieve_tokens(self, text):
+        for text in texts:
+            phrases.append(connection.find('units',
+                           text=text.id, unit_type='phrase'))
+
+        return phrases
+
+    def retrieve_tokens(self, texts):
         """Get the tokens associated with a text from the database.
 
         Parameters
@@ -99,14 +169,33 @@ class DefaultMatcher(object):
         tokens : list of tesserae.db.Token
             The tokens in the order they appear in the original text.
         """
-        pass
+        tokens = []
+
+        for text in texts:
+            tokens.append(connection.find('units', text=text.id))
+
+        return phrases
 
     def score(self, frequencies, distances):
         """
         """
         pass
 
-    def span_dist(self, tokens, match_tokens):
+    def span_dist(self, match_tokens):
+        """Compute distance based on position in the text.
+
+        The span distance computes the number of words separating the first and
+        final match words in the unit.
+
+        Parameters
+        ----------
+        match_tokens : list of (tesserae.db.Token, tesserae.db.Frequency)
+            The tokens to compute the distance.
+
+        Returns
+        -------
+        distance : float
+            The number of words separating the two lowest-frequency tokens.
         """
-        """
-        pass
+        match_tokens.sort(key=lambda x: x[0].index)
+        return np.abs(match_tokens[0].index, match_tokens[1].index)
