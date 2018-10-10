c00bded356826a477c7513c7ec8dc49303e3182f
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-10-10 13:32:00 -0400

    initial unit tests

--- tests/db/test_mongodb.py
+++ tests/db/test_mongodb.py
@@ -148,13 +148,77 @@ class TestTessMongoConnection(object):
                              '$exists': True}}
 
     def test_delete(self, request, populate):
-        pass
+        conf = request.config
+        conn = TessMongoConnection(conf.getoption('db_host'),
+                                   conf.getoption('db_port'),
+                                   conf.getoption('db_user'),
+                                   password=conf.getoption('db_passwd',
+                                                           default=None),
+                                   db=conf.getoption('db_name',
+                                                     default=None))
+
+        for entity_type in populate:
+            for query_ent in populate[entity_type]:
+                res = conn.delete(entity_type, _id=query_ent['_id'])
+                assert res.deleted_count == 1
+                assert res.raw_result == query_ent
+                conn.insert(entity_type, **query_ent)
+
+                conn.delete(entity_type, **query_ent)
+                assert res.deleted_count == 1
+                assert res.raw_result == query_ent
+                conn.insert(entity_type, **query_ent)
 
     def test_find(self, request, populate):
-        pass
+        conf = request.config
+        conn = TessMongoConnection(conf.getoption('db_host'),
+                                   conf.getoption('db_port'),
+                                   conf.getoption('db_user'),
+                                   password=conf.getoption('db_passwd',
+                                                           default=None),
+                                   db=conf.getoption('db_name',
+                                                     default=None))
+        for entity_type in populate:
+            for query_ent in populate[entity_type]:
+                ents = conn.find(entity_type, _id=query_ent['_id'])
+                assert ents[0].json_encode == query_ent
+
+                ents = conn.find(entity_type, **query_ent)
+                assert ents[0].json_encode == query_ent
+
+        for text in populate['texts']:
+            ents = conn.find('tokens', text=text.path)
+            correct_tokens = [t for t in populate['tokens']
+                              if t['text'] == text.path]
+            assert len(ents) == len(correct_tokens)
+
+            ents = conn.find('frequencies', text=text.path)
+            correct_tokens = [t for t in populate['frequencies']
+                              if t['text'] == text.path]
+            assert len(ents) == len(correct_tokens)
+
+            ents = conn.find('units', text=text.path)
+            correct_tokens = [t for t in populate['units']
+                              if t['text'] == text.path]
+            assert len(ents) == len(correct_tokens)
+
 
     def test_insert(self, request, populate):
-        pass
+        conf = request.config
+        conn = TessMongoConnection(conf.getoption('db_host'),
+                                   conf.getoption('db_port'),
+                                   conf.getoption('db_user'),
+                                   password=conf.getoption('db_passwd',
+                                                           default=None),
+                                   db=conf.getoption('db_name',
+                                                     default=None))
+        for entity_type in populate:
+            conn.delete(entity_type, _id=[t['_id'] for t in
+                                          populate[entity_type]])
+            for query_ent in populate[entity_type]:
+                del query_ent['_id']
+                res = conn.insert(entity_type, **query_ent)
+                assert res.inserted_ids[0] == query_ent['_id']
 
     def test_to_query_list(self):
         pass
@@ -163,4 +227,18 @@ class TestTessMongoConnection(object):
         pass
 
     def test_update(self, request, populate):
-        pass
+        conf = request.config
+        conn = TessMongoConnection(conf.getoption('db_host'),
+                                   conf.getoption('db_port'),
+                                   conf.getoption('db_user'),
+                                   password=conf.getoption('db_passwd',
+                                                           default=None),
+                                   db=conf.getoption('db_name',
+                                                     default=None))
+        for entity_type in populate:
+            for query_ent in populate[entity_type]:
+                query_ent['foo'] = 'bar'
+                res = conn.insert(entity_type, **query_ent)
+                assert res.matched_count == 1
+                assert res.modified_count == 1
+                assert res.raw_result == query_ent
