e422635f297f7ddb825c90e520e4994f95abad52
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-10-11 02:35:40 -0400

    add explicit entity class map in lieu of having a better solution

--- tesserae/db/entities/__init__.py
+++ tesserae/db/entities/__init__.py
@@ -5,4 +5,11 @@ from .text import Text
 from .token import Token
 from .unit import Unit
 
+entity_map = {}
+entity_map[Frequency.collection] = Frequency
+entity_map[Match.collection] = Match
+entity_map[Text.collection] = Text
+entity_map[Token.collection] = Token
+entity_map[Unit.collection] = Unit
+
 __all__ = ['Entity', 'Frequency', 'Match', 'Text', 'Token', 'Unit']
