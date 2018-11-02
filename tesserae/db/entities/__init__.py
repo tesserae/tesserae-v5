ba423af01938415570f7898bb4975ab9d6dd2ee1
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2018-11-02 18:24:44 -0400

    Arbitrary stopwords lists

--- tesserae/db/entities/__init__.py
+++ tesserae/db/entities/__init__.py
@@ -4,6 +4,7 @@ from .match import Match
 from .text import Text
 from .token import Token
 from .unit import Unit
+from .swlist import StopwordsList
 
 entity_map = {}
 entity_map[Frequency.collection] = Frequency
@@ -11,5 +12,6 @@ entity_map[Match.collection] = Match
 entity_map[Text.collection] = Text
 entity_map[Token.collection] = Token
 entity_map[Unit.collection] = Unit
+entity_map[StopwordsList.collection] = StopwordsList
 
 __all__ = ['Entity', 'Frequency', 'Match', 'Text', 'Token', 'Unit']
