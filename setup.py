cfe53efa09c618e9a7973f22be73db79b43dd7d9
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-05-02 17:00:49 -0400

    update setup.py to install lemmata models

--- setup.py
+++ setup.py
@@ -1,4 +1,57 @@
+from pathlib import Path
+import os
 from setuptools import setup
+from setuptools.command.install import install
+import shutil
+import sys
+import urllib.request
+import zipfile
+
+
+class InstallLemmataModels(install):
+    """Helper to install CLTK lemmatization models."""
+    description = "Install CLTK lemmatization models to $HOME/cltk_tools"
+
+    def run(self):
+        """Install CLTK lemmatization models.
+
+        Tesserae uses the CLTK lemmatizers for each language, and these have
+        accompanying data that must be installed separately. This function
+        installs them to `$HOME/cltk_data` where they may be found by the
+        lemmatizer.
+        """
+        latin = 'https://github.com/cltk/latin_models_cltk/archive/master.zip'
+        greek = 'https://github.com/cltk/greek_models_cltk/archive/master.zip'
+
+        home = str(Path.home())
+        base = os.path.join(home, 'cltk_data', 'latin', 'model')
+        if not os.path.isdir(base):
+            os.makedirs(base, exist_ok=True)
+
+        fname = os.path.join(base, 'latin_models_cltk.zip')
+        with urllib.request.urlopen(latin) as response, open(fname, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+
+        with zipfile.ZipFile(fname, mode='r') as zf:
+            zf.extractall(base)
+
+        fname, _ = os.path.splitext(fname)
+        os.rename(fname + '-master', fname)
+
+        base = os.path.join(home, 'cltk_data', 'greek', 'model')
+        if not os.path.isdir(base):
+            os.makedirs(base, exist_ok=True)
+
+        fname = os.path.join(base, 'greek_models_cltk.zip')
+        with urllib.request.urlopen(greek) as response, open(fname, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+
+        with zipfile.ZipFile(fname, mode='r') as zf:
+            zf.extractall(base)
+
+        fname, _ = os.path.splitext(fname)
+        os.rename(fname + '-master', fname)
+
 
 setup(
     name='tesserae',
@@ -33,5 +86,6 @@ setup(
         'nltk>=3.2.5',
         'numpy>=1.14.0',
         'pymongo>=3.6.1',
-    ]
+    ],
+    cmdclass={'install': InstallLemmataModels}
 )
