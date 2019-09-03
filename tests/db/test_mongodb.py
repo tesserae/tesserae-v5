c15efa46bed3dcc1bb7f7b0b4da38b34a59e6cb3
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-09-03 16:48:20 -0400

    rewrite unit tests

--- tests/db/test_mongodb.py
+++ tests/db/test_mongodb.py
@@ -1,23 +1,80 @@
+"""Unit tests for the MongoDB connection, operations, and sanitizers.
+
+Fixtures
+--------
+populate
+    Populate the database with test entries.
+depopulate
+    Remove all test entries from the database.
+
+
+Functions
+---------
+test_init
+    Test schemes for initializing TessMongoConnection.
+test_insert
+test_find
+test_update
+test_delete
+    Test CRUD operations mediated by TessMongoConnection.
+test_create_filter
+    Test the sanitizer for creating MongoDB filters.
+
+"""
+
 import pytest
 
-from tesserae.db.mongodb import TessMongoConnection
+from tesserae.db.mongodb import TessMongoConnection, to_query_list, \
+                                to_query_range
 
 import datetime
 
 import pymongo
+import pymongo.results
 
-from tesserae.db.entities import Frequency, Match, Text, Token, Unit
+from tesserae.db.entities import entity_map
 
 
 @pytest.fixture(scope='module')
-def entity_map():
-    return {
-        'frequencies': Frequency,
-        'matches': Match,
-        'texts': Text,
-        'tokens': Token,
-        'units': Unit
-    }
+def depopulate(connection):
+    """Clean up the test database.
+
+    Fixtures
+    --------
+    connection
+        Provides a connection to the test database.
+    """
+    # Stall until the tests run
+    yield None
+
+    # Once tests are finished, delete all entries from the database.
+    for key, val in entity_map.items():
+        collection = entity_map[key].collection
+        connection[collection].delete_many({})
+
+
+@pytest.fixture(scope='module')
+def populate(connection, test_data):
+    """Insert test entries into the database.
+
+    Fixtures
+    --------
+    connection
+        Provides a connection to the test database.
+    test_data
+        Loads the test data for database interaction evaluation.
+    """
+    for key, val in test_data.items():
+        collection = entity_map[key].collection
+        result = connection[collection].insert_many(val)
+        for entry in val:
+            entry['id'] = entry['_id']
+            del entry['_id']
+
+    yield test_data
+
+    # for key in test_data.keys():
+    #     connection[key].delete_many({})
 
 
 def test_init(request):
@@ -161,7 +218,38 @@ def test_create_filter(request):
                          '$exists': True}}
 
 
-def test_delete(request, populate, entity_map):
+def test_insert(request, test_data, depopulate):
+    conf = request.config
+    conn = TessMongoConnection(conf.getoption('db_host'),
+                               conf.getoption('db_port'),
+                               conf.getoption('db_user'),
+                               password=conf.getoption('db_passwd',
+                                                       default=None),
+                               db=conf.getoption('db_name',
+                                                 default=None))
+
+    for key, val in test_data.items():
+        entity = entity_map[key]
+        ents = [entity(**entry) for entry in val]
+
+        # Insert the result for the first time. Should return the standard
+        # pymongo.collection.insert_many result.
+        result = conn.insert(ents)
+
+        assert isinstance(result, pymongo.results.InsertManyResult)
+        for i in range(len(result.inserted_ids)):
+            assert ents[i].id == result.inserted_ids[i]
+
+        # Attempt to insert the same entities a second time. Should return an
+        # empty list.
+        result = conn.insert(ents)
+        assert result == []
+
+        # Clean up
+        conn.connection[key].delete_many({'_id': {'$in': [e.id for e in ents]}})
+
+def test_find(request, populate):
+    conf = request.config
     conn = TessMongoConnection(conf.getoption('db_host'),
                                conf.getoption('db_port'),
                                conf.getoption('db_user'),
@@ -171,26 +259,97 @@ def test_delete(request, populate, entity_map):
                                                  default=None))
 
     for key, val in populate.items():
-        ent = entity_map[key]
+        # Test finding every entry in the database.
+        result = conn.find(key)
+        assert len(result) == len(val)
+        for r in result:
+            match = [all([getattr(r, k) == val[i][k] for k in val[i].keys()])
+                     for i in range(len(val))]
+            assert sum([1 if m else 0 for m in match]) == 1
+
+        # Test finding individual entries
+        for entry in val:
+            result = conn.find(key, _id=entry['id'])
+            assert len(result) == 1
+            assert all([getattr(result[0], k) == entry[k]
+                        for k in entry.keys()])
+
+        # Test finding non-existent entries
+        result = conn.find(key, foo='bar')
+        assert result == []
 
 
+def test_update(request, populate):
+    conf = request.config
+    conn = TessMongoConnection(conf.getoption('db_host'),
+                               conf.getoption('db_port'),
+                               conf.getoption('db_user'),
+                               password=conf.getoption('db_passwd',
+                                                       default=None),
+                               db=conf.getoption('db_name',
+                                                 default=None))
+
+    for key, val in populate.items():
+        entity = entity_map[key]
+        ents = [entity(**entry) for entry in val]
+        for e in ents:
+            e.foo = 'bar'
+
+        result = conn.update(ents)
+        assert result.matched_count == len(val)
+        assert result.modified_count == len(val)
+
+        found = conn.connection[key].find()
+        assert all(['foo' in doc and doc['foo'] == 'bar' for doc in found])
 
 
-def test_find(request, populate, entity_map):
-    pass
+def test_delete(request, populate):
+    conf = request.config
+    conn = TessMongoConnection(conf.getoption('db_host'),
+                               conf.getoption('db_port'),
+                               conf.getoption('db_user'),
+                               password=conf.getoption('db_passwd',
+                                                       default=None),
+                               db=conf.getoption('db_name',
+                                                 default=None))
+    print(conn.connection.name)
 
+    for key, val in populate.items():
+        entity = entity_map[key]
+        ents = [entity(**entry) for entry in val]
 
-def test_insert(request, populate, entity_map):
-    pass
+        # Test deleting one entry
+        result = conn.delete([ents[0]])
+        assert result.deleted_count == 1
+
+        found = conn.connection[key].find({'_id': ents[0].id})
+        assert found.count() == 0
+        found = conn.connection[key].find()
+        print(list(found))
+        assert found.count() == len(val) - 1
+
+        # Test deleting the collection's entries
+        result = conn.delete(ents)
+        assert result.deleted_count == len(val) - 1
+
+        # Ensure that the collection is empty
+        found = conn.connection[key].find()
+        assert found.count() == 0
 
 
 def test_to_query_list():
-    pass
+    assert to_query_list(1) == [1]
+    assert to_query_list(1.0) == [1.0]
+    assert to_query_list('foo') == ['foo']
+    assert to_query_list(True) == [True]
+    assert to_query_list(False) == [False]
+    assert to_query_list(None) == [None]
+    assert to_query_list((1, 2, 3)) == [1, 2, 3]
+    assert to_query_list(['foo', 'bar', 'baz']) == ['foo', 'bar', 'baz']
 
 
 def test_to_query_range():
-    pass
-
-
-def test_update(request, populate, entity_map):
-    pass
+    assert to_query_range(0, 1) == (0, 1)
+    assert to_query_range(1, 0) == (0, 1)
+    assert to_query_range(-100, 1) == (-100, 1)
+    assert to_query_range(0, -111) == (-111, 0)
