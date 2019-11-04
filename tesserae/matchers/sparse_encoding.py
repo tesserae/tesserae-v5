fe0ac46bfb1030d12da63513ee090156b6af71d8
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-11-04 17:47:34 -0500

    remove prints, add parallelism

--- tesserae/matchers/sparse_encoding.py
+++ tesserae/matchers/sparse_encoding.py
@@ -6,6 +6,7 @@ Classes
 """
 import itertools
 import multiprocessing as mp
+from multiprocessing.pool import ThreadPool
 
 import numpy as np
 import pymongo
@@ -103,7 +104,6 @@ class SparseMatrixSearch(object):
 
         stoplist = self.connection.aggregate(Feature.collection, pipeline, encode=False)
         stoplist = list(stoplist)
-        print([s['token'] for s in stoplist])
         return np.array([s['index'] for s in stoplist], dtype=np.uint32)
 
     def get_frequencies(self, feature, language, basis='corpus', count=None):
@@ -149,7 +149,7 @@ class SparseMatrixSearch(object):
     def match(self, texts, unit_type, feature, stopwords=10,
               stopword_basis='corpus', score_basis='word',
               frequency_basis='texts', max_distance=10,
-              distance_metric='frequency', min_score=6):
+              distance_metric='frequency', min_score=6, parallel=True):
         """Find matches between one or more texts.
 
         Texts will contain lines or phrases with matching tokens, with varying
@@ -239,7 +239,6 @@ class SparseMatrixSearch(object):
                     unit_indices.extend([unit['index'] for _ in range(len(all_features))])
                     feature_indices.extend(all_features)
 
-            # print(feature_indices)
             feature_matrix = dok_matrix((len(units), feature_count))
             feature_matrix[(np.asarray(unit_indices), np.asarray(feature_indices))] = 1
 
@@ -262,37 +261,42 @@ class SparseMatrixSearch(object):
 
         match_ents = []
         match_set = MatchSet(texts=texts)
-        # with mp.Pool() as p:
-        #    result = p.map(score_wrapper, [(source_unit, units[1], frequencies, distance_metric, max_distance, -np.inf) for source_unit in units[0]])
-        for i in range(matches.shape[0]):
-            source_unit = unit_lists[0][i]
-            target_units = [unit_lists[1][j] for j in matches[i].nonzero()[1]]
-            match_ents.extend(score(source_unit, target_units, frequencies, features, distance_metric, max_distance, min_score, match_set))
+        if parallel:
+            with ThreadPool() as p:
+                match_ents = p.starmap(score, [(unit_lists[0][i], [unit_lists[1][j] for j in matches[i].nonzero()[1]],
+                                            frequencies, features, distance_metric,
+                                            max_distance, min_score, match_set)
+                                            for i in range(matches.shape[0])])
+            match_ents = list(itertools.chain.from_iterable(match_ents))
+        else:
+            for i in range(matches.shape[0]):
+                source_unit = unit_lists[0][i]
+                target_units = [unit_lists[1][j] for j in matches[i].nonzero()[1]]
+                match_ents.extend(score(source_unit, target_units, frequencies, features, distance_metric, max_distance, min_score, match_set))
 
+        entmax = np.max([m.score for m in match_ents])
+        entmin = np.min([m.score for m in match_ents])
+        for ent in match_ents:
+            ent.score = int(np.ceil(10 * (ent.score - entmin) / (entmax - entmin)))
 
+        match_ents = [m for m in match_ents if m.score >= min_score]
 
         return match_ents, match_set
 
 
-def score_wrapper(args):
-    return score(*args)
-
-
 def score(source, targets, frequencies, features, distance_metric, maximum_distance, min_score, match_set):
     matches = []
-    source_features = list(itertools.chain.from_iterable(source['features'][0]['features']))  # list(np.array(source['features'][0]['features']).ravel())
-    # print(source['features'][0])
+    source_features = list(itertools.chain.from_iterable(source['features'][0]['features']))
     source_indices = list([[source['features'][0]['index'][i] for _ in range(len(source['features'][0]['features'][i]))] for i in range(len(source['features'][0]['index']))])
-    source_indices = list(itertools.chain.from_iterable(source_indices))  # list(np.array(source_indices).ravel())
+    source_indices = list(itertools.chain.from_iterable(source_indices))
     source_frequencies = np.array([frequencies[i] for i in source_features])
 
     for target in targets:
-        target_features = list(itertools.chain.from_iterable(target['features'][0]['features']))  # list(np.array(target['features'][0]['features']).ravel())
+        target_features = list(itertools.chain.from_iterable(target['features'][0]['features']))
         target_indices = list([[target['features'][0]['index'][i] for _ in range(len(target['features'][0]['features'][i]))] for i in range(len(target['features'][0]['index']))])
-        target_indices = list(itertools.chain.from_iterable(target_indices))  # list(np.array(target_indices).ravel())
+        target_indices = list(itertools.chain.from_iterable(target_indices))
         target_frequencies = np.array([frequencies[i] for i in target_features])
 
-        # print(source_features, target_features)
         match_features = set(source_features).intersection(set(target_features))
         match_idx = [source_features.index(i) for i in match_features]
         match_idx = [source_indices[i] for i in match_idx]
@@ -307,34 +311,26 @@ def score(source, targets, frequencies, features, distance_metric, maximum_dista
                 target_distance = np.abs(np.max(target_idx) - np.min(target_idx)) + 1
             else:
                 freq_sort = np.argsort(source_frequencies)
-                # print(freq_sort.shape[0], len(source_features), len(source_indices))
                 source_idx = np.array([source_indices[i] for i in freq_sort])
-                # try:
                 if source_idx.shape[0] > 2:
                     s_end = [s for s in source_idx if s != source_idx[0]][0]
                 else:
                     continue
-                # except IndexError:
-                #     s_end = 1
+
                 freq_sort = np.argsort(target_frequencies)
                 target_idx = np.array([target_indices[i] for i in freq_sort])
-                # try:
+
                 if target_idx.shape[0] > 2:
                     t_end = [t for t in target_idx if t != target_idx[0]][0]
                 else:
                     continue
-                # except IndexError:
-                #     t_end = 1
+
                 source_distance = np.abs(s_end - source_idx[0]) + 1
                 target_distance = np.abs(t_end - target_idx[0]) + 1
 
             score = -np.inf
             if source_distance < maximum_distance and target_distance < maximum_distance:
                 score = np.log((np.power(np.sum(match_frequencies), -1) * 2) / (source_distance + target_distance))
-                # print('Score: {}'.format(score))
-
-            if score >= min_score:
-                # print('Adding match')
                 matches.append(
                     Match(
                         units=[source['_id'], target['_id']],
