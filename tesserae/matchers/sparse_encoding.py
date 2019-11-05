ec1bc21884dc15ee22977a50d3cbaf3f2374b894
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2019-11-04 19:27:23 -0500

    Bug fix: scoring
    I think I've made scoring work as detailed in Figure 1 of Forstall et
    al., Modeling the Scholars..., 2014.  Unfortunately, the lemmatizer
    we're using generates different results than v3.  Might need to swap in
    the v3 lemmatizer just to be sure that scoring is being done correctly.

--- tesserae/matchers/sparse_encoding.py
+++ tesserae/matchers/sparse_encoding.py
@@ -7,11 +7,12 @@ Classes
 import itertools
 import multiprocessing as mp
 
+from bson import ObjectId
 import numpy as np
 import pymongo
 from scipy.sparse import dok_matrix
 
-from tesserae.db.entities import Entity, Feature, Match, MatchSet, Unit
+from tesserae.db.entities import Entity, Feature, Match, MatchSet, Token, Text, Unit
 
 
 class SparseMatrixSearch(object):
@@ -107,45 +108,19 @@ class SparseMatrixSearch(object):
         print([(s['token'], s['frequency']) for s in stoplist])
         return np.array([s['index'] for s in stoplist], dtype=np.uint32)
 
-    def get_frequencies(self, feature, language, basis='corpus', count=None):
+    def get_frequencies(self, feature, language, basis='corpus'):
         """Get frequency data for a given feature.
+
+        Frequency is equal to the number of times a feature occurs in the basis
+        divided by the total number of tokens in the basis.
+
+        Note that the sum of all features is not equivalent to the sum of all
+        tokens, since every token can have multiple instances of the same
+        feature type associated with it.
         """
-        pipeline = [
-            {'$match': {'feature': feature, 'language': language}}
-        ]
-
-        if basis == 'corpus':
-            pipeline.append(
-                {'$project': {
-                    '_id': False,
-                    'index': True,
-                    'frequency': {
-                        '$reduce': {
-                            'input': {'$objectToArray': '$frequencies'},
-                            'initialValue': 0,
-                            'in': {'$sum': ['$$value', '$$this.v']}
-                        }
-                    }
-                }})
-        else:
-            basis = [t if not isinstance(t, Entity) else str(t.id) for t in basis]
-            pipeline.extend([
-                {'$project': {
-                    '_id': False,
-                    'index': True,
-                    'token': True,
-                    'frequency': {'$sum': ['$frequencies.' + text for text in basis]}
-                }}
-            ])
-
-        pipeline.extend([
-            {'$sort': {'index': 1}}
-        ])
-
-        freqs = self.connection.aggregate(Feature.collection, pipeline, encode=False)
-        freqs = list(freqs)
-        freqs = np.array([freq['frequency'] for freq in freqs])
-        return freqs / sum(freqs)
+        freqs = _get_feature_counts(self.connection, feature, language, basis)
+        token_count = _get_token_count(self.connection, language, basis)
+        return freqs / token_count
 
     def match(self, texts, unit_type, feature, stopwords=10,
               stopword_basis='corpus', score_basis='word',
@@ -199,9 +174,6 @@ class SparseMatrixSearch(object):
             stoplist = self.get_stoplist(stopwords,
                     'form' if feature == 'form' else 'lemmata',
                     texts[0].language)
-
-        frequency_basis = frequency_basis if frequency_basis != 'texts' \
-                          else texts
 
         match_matrices = []
 
@@ -262,8 +234,18 @@ class SparseMatrixSearch(object):
         matches[matches == 1] = 0
         matches.eliminate_zeros()
 
-        frequencies = self.get_frequencies(feature, texts[0].language, basis=frequency_basis)
+
+        if frequency_basis != 'texts':
+            source_frequencies = self.get_frequencies(feature,
+                    texts[0].language, basis=frequency_basis)
+            target_frequencies = source_frequencies
+        else:
+            source_frequencies = self.get_frequencies(feature,
+                    texts[0].language, basis=[texts[0]])
+            target_frequencies = self.get_frequencies(feature,
+                    texts[1].language, basis=[texts[1]])
         features = sorted(self.connection.find('features', language=texts[0].language, feature=feature), key=lambda x: x.index)
+        stoplist_set = set(stoplist)
 
         match_ents = []
         match_set = MatchSet(texts=texts)
@@ -272,7 +254,9 @@ class SparseMatrixSearch(object):
         for i in range(matches.shape[0]):
             source_unit = unit_lists[0][i]
             target_units = [unit_lists[1][j] for j in matches[i].nonzero()[1]]
-            match_ents.extend(score(source_unit, target_units, frequencies, features, distance_metric, max_distance, min_score, match_set))
+            match_ents.extend(score(source_unit, target_units,
+                source_frequencies, target_frequencies,
+                features, stoplist_set, distance_metric, max_distance, min_score, match_set))
 
 
 
@@ -283,61 +267,116 @@ def score_wrapper(args):
     return score(*args)
 
 
-def score(source, targets, frequencies, features, distance_metric, maximum_distance, min_score, match_set):
+def score(source, targets, in_source_frequencies, in_target_frequencies,
+        features, stoplist_set, distance_metric, maximum_distance, min_score, match_set):
     matches = []
+    '''
+    ``source`` is a dictionary with the following keys:
+        _id: matches a Unit.id in the database representing a unit in the
+            source text
+        index: an int corresponding to the index of the Unit with same id as
+            _id
+        features: a list containing a single dictionary
+
+    the dictionary under ``source['features']`` has the following keys:
+        index: a list of ints corresponding to Token indices
+        features: a list of lists; each position in the outer list corresponds
+            to the same position in ``source['features'][0]['index']``; the
+            inner list contains ints corresponding to Feature indices; the type
+            of Feature being referred to was determined by the match() call
+    '''
+    # print(source)
+    '''
+    ``source_features`` is a flattened version of
+    ``source['features'][0]['features'] (i.e., the list of lists has become a
+    list)
+    '''
     source_features = list(itertools.chain.from_iterable(source['features'][0]['features']))  # list(np.array(source['features'][0]['features']).ravel())
-    # print(source['features'][0])
-    source_indices = list([[source['features'][0]['index'][i] for _ in range(len(source['features'][0]['features'][i]))] for i in range(len(source['features'][0]['index']))])
+    source_features_set = set(source_features)
+    '''
+    ``source_indices[i]`` is equal to the token position in the unit to which
+    ``source_features[i]`` corresponds
+    '''
+    source_indices = list([[i for _ in range(len(source['features'][0]['features'][i]))] for i in range(len(source['features'][0]['index']))])
     source_indices = list(itertools.chain.from_iterable(source_indices))  # list(np.array(source_indices).ravel())
-    source_frequencies = np.array([frequencies[i] for i in source_features])
+    source_frequencies = np.array([in_source_frequencies[i] for i in source_features])
 
     for target in targets:
         target_features = list(itertools.chain.from_iterable(target['features'][0]['features']))  # list(np.array(target['features'][0]['features']).ravel())
-        target_indices = list([[target['features'][0]['index'][i] for _ in range(len(target['features'][0]['features'][i]))] for i in range(len(target['features'][0]['index']))])
+        target_indices = list([[i for _ in range(len(target['features'][0]['features'][i]))] for i in range(len(target['features'][0]['index']))])
         target_indices = list(itertools.chain.from_iterable(target_indices))  # list(np.array(target_indices).ravel())
-        target_frequencies = np.array([frequencies[i] for i in target_features])
+        target_frequencies = np.array([in_target_frequencies[i] for i in target_features])
 
         # print(source_features, target_features)
-        match_features = set(source_features).intersection(set(target_features))
+        match_features = \
+                source_features_set.intersection(
+                        set(target_features)).difference(stoplist_set)
         match_idx = [source_features.index(i) for i in match_features]
         match_idx = [source_indices[i] for i in match_idx]
 
         if not match_idx.count(match_idx[0]) == len(match_idx) and match_idx:
-            match_frequencies = [frequencies[i] for i in match_features]
+            match_frequencies = \
+                    [in_target_frequencies[i] for i in match_features] + \
+                    [in_source_frequencies[i] for i in match_features]
+
+            print('target frequencies', [(in_target_frequencies[i], i) for i in match_features])
+            print('target positions', target['features'][0]['features'])
+            print('source frequencies', [(in_source_frequencies[i], i) for i in match_features])
+            print('source positions', source['features'][0]['features'])
 
             if distance_metric == 'span':
                 source_idx = np.array([source_indices[i] for i in [source_features.index(f) for f in match_features]])
                 target_idx = np.array([target_indices[i] for i in [target_features.index(f) for f in match_features]])
-                source_distance = np.abs(np.max(source_idx) - np.min(source_idx)) + 1
-                target_distance = np.abs(np.max(target_idx) - np.min(target_idx)) + 1
+                source_distance = np.abs(np.max(source_idx) - np.min(source_idx))
+                target_distance = np.abs(np.max(target_idx) - np.min(target_idx))
             else:
-                freq_sort = np.argsort(source_frequencies)
+                matched_source_frequencies = [
+                        source_frequencies[i]
+                        for i, feature in enumerate(source_features)
+                        if feature in match_features]
+                matched_source_indices = [
+                        source_indices[i]
+                        for i, feature in enumerate(source_features)
+                        if feature in match_features]
+                freq_sort = np.argsort(matched_source_frequencies)
                 # print(freq_sort.shape[0], len(source_features), len(source_indices))
-                source_idx = np.array([source_indices[i] for i in freq_sort])
+                source_idx = np.array([matched_source_indices[i] for i in freq_sort])
+                print('source_idx', source_idx)
                 # try:
-                if source_idx.shape[0] > 2:
+                if source_idx.shape[0] >= 2:
                     s_end = [s for s in source_idx if s != source_idx[0]][0]
                 else:
                     continue
                 # except IndexError:
                 #     s_end = 1
-                freq_sort = np.argsort(target_frequencies)
-                target_idx = np.array([target_indices[i] for i in freq_sort])
+                matched_target_frequencies = [
+                        target_frequencies[i]
+                        for i, feature in enumerate(target_features)
+                        if feature in match_features]
+                matched_target_indices = [
+                        target_indices[i]
+                        for i, feature in enumerate(target_features)
+                        if feature in match_features]
+                freq_sort = np.argsort(matched_target_frequencies)
+                # print(freq_sort.shape[0], len(target_features), len(target_indices))
+                target_idx = np.array([matched_target_indices[i] for i in freq_sort])
+                print('target_idx', target_idx)
                 # try:
-                if target_idx.shape[0] > 2:
+                if target_idx.shape[0] >= 2:
                     t_end = [t for t in target_idx if t != target_idx[0]][0]
                 else:
                     continue
                 # except IndexError:
                 #     t_end = 1
-                source_distance = np.abs(s_end - source_idx[0]) + 1
-                target_distance = np.abs(t_end - target_idx[0]) + 1
+                source_distance = np.abs(s_end - source_idx[0])
+                target_distance = np.abs(t_end - target_idx[0])
 
             score = -np.inf
             if source_distance < maximum_distance and target_distance < maximum_distance:
-                score = np.log((np.power(np.sum(match_frequencies), -1) * 2) / (source_distance + target_distance))
+                score = np.log((np.sum(np.power(match_frequencies, -1))) / (source_distance + target_distance))
                 # print('Score: {}'.format(score))
 
+            print(score, target_distance, source_distance)
             if score >= min_score:
                 # print('Adding match')
                 matches.append(
@@ -348,3 +387,56 @@ def score(source, targets, frequencies, features, distance_metric, maximum_dista
                         match_set=match_set))
 
     return matches
+
+
+def _get_feature_counts(connection, feature, language, basis):
+    """
+    """
+    pipeline = [
+        {'$match': {'feature': feature, 'language': language}}
+    ]
+
+    if basis == 'corpus':
+        pipeline.append(
+            {'$project': {
+                '_id': False,
+                'index': True,
+                'frequency': {
+                    '$reduce': {
+                        'input': {'$objectToArray': '$frequencies'},
+                        'initialValue': 0,
+                        'in': {'$sum': ['$$value', '$$this.v']}
+                    }
+                }
+            }})
+    else:
+        basis = [t if not isinstance(t, Entity) else str(t.id) for t in basis]
+        pipeline.extend([
+            {'$project': {
+                '_id': False,
+                'index': True,
+                'token': True,
+                'frequency': {'$sum': ['$frequencies.' + text for text in basis]}
+            }}
+        ])
+
+    pipeline.extend([
+        {'$sort': {'index': 1}}
+    ])
+
+    freqs = connection.aggregate(Feature.collection, pipeline, encode=False)
+    freqs = list(freqs)
+    freqs = np.array([freq['frequency'] for freq in freqs])
+    return freqs
+
+
+def _get_token_count(connection, language, basis):
+    if basis == 'corpus':
+        text_ids = [ObjectId(t.id) for t in connection.find(Text.collection,
+            language=language)]
+    else:
+        text_ids = [ObjectId(t.id) if isinstance(t, Entity) else ObjectId(t) for t in basis]
+    return connection.connection[Token.collection].count_documents(
+            {'text': {'$in': text_ids},
+                # https://stackoverflow.com/a/6838057
+                'features': {'$gt': {}}})
