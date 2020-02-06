ccc0c53f65b36a95f1d2d10239ef04cc2471d7d0
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2020-02-06 13:48:59 -0500

    New database entity: Property
    A Property belongs to a Unit and is identified by its position within
    the Unit and a Feature instance present at that position.

--- tesserae/db/entities/__init__.py
+++ tesserae/db/entities/__init__.py
@@ -1,6 +1,7 @@
 from .entity import Entity
 from .feature import Feature
 from .match import Match
+from .property import Property
 from .search import Search
 from .swlist import StopwordsList
 from .text import Text
@@ -10,11 +11,12 @@ from .unit import Unit
 entity_map = {}
 entity_map[Feature.collection] = Feature
 entity_map[Match.collection] = Match
+entity_map[Property.collection] = Property
 entity_map[Search.collection] = Search
 entity_map[StopwordsList.collection] = StopwordsList
 entity_map[Text.collection] = Text
 entity_map[Token.collection] = Token
 entity_map[Unit.collection] = Unit
 
-__all__ = ['Entity', 'Feature', 'Match', 'Search', 'Text',
+__all__ = ['Entity', 'Feature', 'Match', 'Property', 'Search', 'Text',
            'Token', 'Unit']
