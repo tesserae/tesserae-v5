89e7c4afe530a3e587d90a3cbbe72a684d430178
Steph <steph@ophelia.home>
2020-04-08 19:07:49 -0400

    Updated setup.py to allow develop mode installations

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 from setuptools import setup
 from setuptools.command.install import install
+from setuptools.command.develop import develop
 import shutil
 import sys
 import urllib.request
@@ -10,7 +11,7 @@ import zipfile
 
 class InstallLemmataModels(install):
     """Helper to install CLTK lemmatization models."""
-    description = "Install CLTK lemmatization models to $HOME/cltk_tools"
+    description = "Install CLTK lemmatization models to $HOME/cltk_data"
 
     def run(self):
         """Install CLTK lemmatization models.
@@ -23,7 +24,6 @@ class InstallLemmataModels(install):
         latin = 'https://github.com/cltk/lat_models_cltk/archive/master.zip'
         greek = 'https://github.com/cltk/grc_models_cltk/archive/master.zip'
         home = str(Path.home())
-
 
         try:
             # Set up the file paths and directories for the Latin models
@@ -67,6 +67,65 @@ class InstallLemmataModels(install):
         # Run the standard installer
         install.run(self)
 
+class DevelopLemmataModels(develop):
+    """Helper to install CLTK lemmatization models."""
+    description = "Install CLTK lemmatization models to $HOME/cltk_data"
+
+    def run(self):
+        """Install CLTK lemmatization models.
+
+        Tesserae uses the CLTK lemmatizers for each language, and these have
+        accompanying data that must be installed separately. This function
+        installs them to `$HOME/cltk_data` where they may be found by the
+        lemmatizer.
+        """
+        latin = 'https://github.com/cltk/lat_models_cltk/archive/master.zip'
+        greek = 'https://github.com/cltk/grc_models_cltk/archive/master.zip'
+        home = str(Path.home())
+
+        try:
+            # Set up the file paths and directories for the Latin models
+            base = os.path.join(home, 'cltk_data', 'lat', 'model')
+            if not os.path.isdir(base):
+                os.makedirs(base, exist_ok=True)
+
+            # Download the Latin models and move the ZIP archive
+            fname = os.path.join(base, 'lat_models_cltk.zip')
+            with urllib.request.urlopen(latin) as response, open(fname, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+
+            # Extract all files from the ZIP archive
+            with zipfile.ZipFile(fname, mode='r') as zf:
+                zf.extractall(base)
+
+            # Rename the extracted directory to match the expected name
+            fname, _ = os.path.splitext(fname)
+            os.rename(fname + '-master', fname)
+        except OSError:
+            pass
+
+        try:
+            # Repeat the process with Greek models
+            base = os.path.join(home, 'cltk_data', 'grc', 'model')
+            if not os.path.isdir(base):
+                os.makedirs(base, exist_ok=True)
+
+            fname = os.path.join(base, 'grc_models_cltk.zip')
+            with urllib.request.urlopen(greek) as response, open(fname, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+
+            with zipfile.ZipFile(fname, mode='r') as zf:
+                zf.extractall(base)
+
+            fname, _ = os.path.splitext(fname)
+            os.rename(fname + '-master', fname)
+        except OSError:
+            pass
+
+        # Run the standard installer
+        develop.run(self)
+
+
 
 setup(
     name='tesserae',
@@ -81,7 +140,8 @@ setup(
               'tesserae.db.entities',
               'tesserae.matchers',
               'tesserae.tokenizers',
-              'tesserae.utils'],
+              'tesserae.utils',
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Research',
@@ -105,5 +165,8 @@ setup(
         'scipy',
         'tqdm',
     ],
-    cmdclass={'install': InstallLemmataModels}
+    cmdclass={
+        'install': InstallLemmataModels,
+        'develop': DevelopLemmataModels,
+    }
 )
