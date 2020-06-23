f92f283bb77475506b20cebd0e3b51a5710a8f46
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2020-06-23 10:31:52 -0400

    Script for initial database setup
    The entries of the synonym dictionaries will need to be pre-registered
    with the database in order for them to work with the current ingestion
    and searching algorithms.
    
    This initial setup script also allows for indexing the database.

--- tesserae/data/__init__.py
+++ tesserae/data/__init__.py
@@ -9,4 +9,12 @@ def _load_data_dictionary(filename):
 
 
 def load_greek_to_latin():
+    """Retrieves Greek to Latin translation dictionary
+
+    Returns
+    -------
+    dict[str, Any]
+        Maps Greek lemma to a tuple of Latin synonyms; these Latin synonyms are
+        Latin lemmata
+    """
     return _load_data_dictionary('g_l.pickle.gz')
