566d3c3eea3c5d29d8f43d3c617703ee04382b06
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2020-09-08 18:05:09 -0400

    BROKEN STATE (STILL): progress on post-ingest featurizing

---
+++ tesserae/features/__init__.py
@@ -0,0 +1,15 @@
+from . import trigrams
+
+
+def get_featurizer(language, feature):
+    """Get appropriate feature extraction callable
+
+    Callable will take list[str] as input and give list[list[str]] as output,
+    such that the list at position x in the output are the features extracted
+    from the str at position x in the input.
+    """
+    if feature == 'sound':
+        return trigrams.trigrammify
+    raise ValueError(
+        f'Could not find a featurizer for {language} to extract features of '
+        f'type {feature}')
--- tesserae/utils/ingest.py
+++ tesserae/utils/ingest.py
@@ -5,6 +5,7 @@ from natsort import natsorted
 
 from tesserae.db.entities import Feature, Unit
 from tesserae.db.entities.text import Text, TextStatus
+from tesserae.features import get_featurizer
 from tesserae.tokenizers import GreekTokenizer, LatinTokenizer
 from tesserae.tokenizers.base import create_features
 from tesserae.unitizer import Unitizer
@@ -249,24 +250,152 @@ def add_feature(connection, text, feature, enable_multitext=False):
     enable_multitext : bool (default: False)
         Whether to enable multitext search with this text
     """
-    for unit, features in gen_updated_unit_and_features(
-            connection, text, feature):
-        feature_cache = {(f.feature, f.token): f
-                         for f in connection.find(Feature.collection,
-                                                  language=text.language)}
-        features_for_insert = []
-        features_for_update = []
+    # TODO add check to make sure feature hasn't already been computed for this
+    # text
+    index_to_form = {
+        f.index: f.token
+        for f in connection.find(
+            Feature.collection, language=text.language, feature='form')
+    }
+    # use lines to compute new features
+    _update_lines_and_features(connection, text, feature, index_to_form)
+    db_feature_cache = {
+        f.token: f
+        for f in connection.find(
+            Feature.collection, language=text.language, feature=feature)
+    }
+    # update other entities, but don't count frequencies again
+    update_phrases(connection, text, feature, index_to_form, db_feature_cache)
+    update_tokens(connection, text, feature, index_to_form, db_feature_cache)
 
-        for f in features:
-            if (f.feature, f.token) not in feature_cache:
-                features_for_insert.append(f)
-                feature_cache[(f.feature, f.token)] = f
-            else:
-                f.id = feature_cache[(f.feature, f.token)].id
-                features_for_update.append(f)
-        connection.insert(features_for_insert)
-        connection.update(features_for_update)
-        connection.update(unit)
+    features_for_insert = []
+    features_for_update = []
+
+    for f in features:
+        if (f.feature, f.token) not in feature_cache:
+            features_for_insert.append(f)
+            feature_cache[(f.feature, f.token)] = f
+        else:
+            f.id = feature_cache[(f.feature, f.token)].id
+            features_for_update.append(f)
+    connection.insert(features_for_insert)
+    connection.update(features_for_update)
+    connection.update(unit)
+    # TODO indicate that feature has been computed
+
+
+def _update_lines_and_features(connection, text, feature, index_to_form):
+    """Compute features and update both lines and features in the database
+
+    Parameters
+    ----------
+    connection : tesserae.db.TessMongoConnection
+        A connection to the database
+    text : tesserae.db.entities.Text
+        The text to update with a new feature
+    feature : str
+        The type of feature to extract and account for from the text
+    index_to_form : dict[int, str]
+        Mapping between form index and form token
+    """
+    lines = connection.find(Unit.collection, text=text.id, unit_type='line')
+    form_indices_encountered = list(
+        set(token['features']['form'][0] for line in lines
+            for token in line.tokens))
+    form_index_to_raw_features = {
+        form_index: raw_features
+        for form_index, raw_features in zip(
+            form_indices_encountered,
+            get_featurizer(text.language, feature)([
+                index_to_form[form_index]
+                for form_index in form_indices_encountered
+            ]))
+    }
+    _update_features(form_index_to_raw_features)
+    db_feature_cache = {
+        f.token: f
+        for f in connection.find(
+            Feature.collection, language=text.language, feature=feature)
+    }
+
+
+def _update_features(connection, text, feature, form_index_to_raw_features):
+    """Update database with Feature information
+
+    This is where frequency information is computed.
+
+    Parameters
+    ----------
+    connection : tesserae.db.TessMongoConnection
+        A connection to the database
+    text : tesserae.db.entities.Text
+        The text to update with a new feature
+    feature : str
+        The type of feature to extract and account for from the text
+    form_index_to_raw_features : dict[int, list[str]]
+        Mapping between form index and list strings extracted as raw features
+        for that form
+    """
+    language = text.language
+    text_id_str = str(text.id)
+    db_feature_cache = {
+        f.token: f
+        for f in connection.find(
+            Feature.collection, language=language, feature=feature)
+    }
+    token_to_features_for_insert = {}
+    token_to_features_for_update = {}
+    for values in form_index_to_raw_features.values():
+        for token in values:
+            if token:
+                if token not in db_feature_cache:
+                    if token not in token_to_features_for_insert:
+                        index = len(db_feature_cache) + \
+                            len(token_to_features_for_insert)
+                        token_to_features_for_insert[token] = Feature(
+                            feature=feature,
+                            token=token,
+                            language=language,
+                            index=index,
+                            frequencies={text_id_str: 1})
+                    else:
+                        cur_feature = token_to_features_for_insert[token]
+                        cur_feature.frequencies[text_id_str] += 1
+                else:
+                    if token not in token_to_features_for_update:
+                        token_to_features_for_update[token] = \
+                            db_feature_cache[token]
+                    cur_feature = token_to_features_for_update[token]
+                    frequencies = cur_feature.frequencies
+                    if text_id_str not in frequencies:
+                        frequencies[text_id_str] = 1
+                    else:
+                        frequencies[text_id_str] += 1
+    connection.insert([f for f in token_to_features_for_insert.values()])
+    connection.update([f for f in token_to_features_for_update.values()])
+
+
+def get_updated_units(connection, text, feature, unit_type):
+    """Compute features and updated units for the text
+
+    Parameters
+    ----------
+    connection : tesserae.db.TessMongoConnection
+        A connection to the database
+    text : tesserae.db.entities.Text
+        The text to update with a new feature
+    feature : str
+        The type of feature to extract and account for from the text
+    unit_type : str
+        The type of unit
+
+    Returns
+    -------
+    list of Unit
+        Every Unit in this list is associated with ``text`` and contains the
+        new feature information
+    """
+    units = connection.find(Unit.collection, text=text.id, unit_type=unit_type)
 
 
 def gen_updated_unit_and_features(connection, text, feature):
